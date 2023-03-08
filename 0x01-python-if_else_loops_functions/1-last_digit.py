6ee6ad48c932c06c81fbb56f66a8f736ad6294cf
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 <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>
2023-03-07 20:10:19 -0800

    remove text line

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print("the digit is {}".format(number))
 if number < 0:
     last_digit = int(repr(number)[-1])
     last_digit = last_digit * -1
