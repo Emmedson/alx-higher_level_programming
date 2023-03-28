0dd64a724082970b83d7a650658270d5447006d9
Emmedson <emmanuel@webcity.com.ng>
2023-03-28 04:52:05 +0100

    Create 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    total = 0
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+            total += 1
+        except IndexError:
+            break
+    print()
+    return(total)
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Priva
